05ad089b789a45a7d1d0a3806ff44a0cac6dca45
estherniyo <estherniyonkuru123@gmail.com>
2023-05-17 16:49:04 +0200

    worked on everythign as far as the desktop is concerned but without the end of the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBrown: "#952626",
+      },
+    },
   },
   plugins: [],
 };
